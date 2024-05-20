e312557270b122d869366df6d0c96a9de3e0dd2d
aryansharma2k4 <aryansharma2k4@gmail.com>
2024-05-20 20:29:43 +0530

    Basics of ReactJS

---
+++ basics/src/App.jsx
@@ -0,0 +1,11 @@
+import Ele from "./ele"
+import Hello from "./hello."
+function App() {
+  return (
+    <>
+    <Ele />
+    <Hello/>
+    </>
+  )
+}
+export default App
---
+++ basics/src/ele.jsx
@@ -0,0 +1,8 @@
+function Ele(){
+      return(
+            <h3>
+                  aryan says' hello
+            </h3>
+      )
+}
+export default Ele
