dc56d40d87d20bb77ce0b68e97eba20559402576
Aryan Sharma <aryansharma2k4@gmail.com>
2024-06-12 14:52:03 +0530

    React Redux

---
+++ Redux/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './assets/components/AddTodo'
+
+function App() {
+
+  return (
+    <>
+    <h1>Learn about redux toolkit</h1>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
